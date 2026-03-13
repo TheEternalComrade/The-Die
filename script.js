1742c9cc13bf1fe6bd040b88d9308eba1ae0e921
TheEternalComrade <taconora52@gmail.com>
2026-03-13 20:54:05 +0000

    everything but the generator

---
+++ script.js
@@ -0,0 +1,8 @@
+const number = document.getElementById("number")
+const result = document.getElementById("result")
+const generateBtn = document.getElementById("generatebtn")
+const list = ["potato", "popotato", "popopotato","popopopotato"]
+let ranInt
+
+
+
