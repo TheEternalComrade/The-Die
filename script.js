40280214c19d5e3fee46b7c0940c7d0734ca70e2
TheEternalComrade <taconora52@gmail.com>
2026-03-13 22:27:13 +0000

    list edition

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const generateBtn = document.getElementById("generatebtn")
 const list = ["potato", "popotato", "popopotato","popopopotato"]
 let ranInt
 
+generateBtn.addEventListener("click", generateNumber)
+
+function generateNumber(){
+    ranInt = Math.floor(Math.random()*4)
+    number.textContent = ranInt
+    result.textContent = list[ranInt]
+}
 
 
